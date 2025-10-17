b4f2d21df078ddb4c8af25d9cbf0c2e8e6b2d3cd
RajaSunrise <RajaSunrise>
2025-10-17 18:18:22 +0800

    fix: error compile binner for windows and split file

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 set -e
 
 # Detect OS and architecture
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+OS_TYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-case $OS in
-    linux)
+case $OS_TYPE in
+    gnu/linux)
+        OS="linux"
+        EXT=""
         case $ARCH in
             x86_64) SUFFIX="linux-x64" ;;
             aarch64) SUFFIX="linux-arm64" ;;
@@ -15,22 +17,34 @@ case $OS in
         esac
         ;;
     darwin)
+        OS="darwin"
+        EXT=""
         case $ARCH in
             x86_64) SUFFIX="darwin-x64" ;;
             arm64) SUFFIX="darwin-arm64" ;;
             *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
         esac
         ;;
+    msys)
+        OS="windows"
+        EXT=".exe"
+        case $ARCH in
+            x86_64) SUFFIX="windows-x64" ;;
+            *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+        esac
+        ;;
     *)
-        echo "Unsupported OS: $OS"
+        echo "Unsupported OS: $OS_TYPE"
         exit 1
         ;;
 esac
 
+BINARY_NAME="goback$EXT"
+
 # Get latest release URL
 REPO="NarmadaWeb/goback"
 LATEST_URL="https://api.github.com/repos/$REPO/releases/latest"
-DOWNLOAD_URL=$(curl -s $LATEST_URL | grep "browser_download_url.*goback-$SUFFIX" | cut -d '"' -f 4)
+DOWNLOAD_URL=$(curl -s $LATEST_URL | grep "browser_download_url.*goback-$SUFFIX$EXT" | cut -d '"' -f 4)
 
 if [ -z "$DOWNLOAD_URL" ]; then
     echo "Could not find download URL for $SUFFIX"
@@ -39,16 +53,23 @@ fi
 
 # Download and install
 echo "Downloading goback for $SUFFIX..."
-curl -L -o goback "$DOWNLOAD_URL"
+curl -L -o "$BINARY_NAME" "$DOWNLOAD_URL"
 
-chmod +x goback
+chmod +x "$BINARY_NAME"
 
 # Install to /usr/local/bin if possible, else ~/bin
-if [ -w /usr/local/bin ]; then
-    sudo mv goback /usr/local/bin/
+if [ "$OS" = "linux" ] || [ "$OS" = "darwin" ]; then
+    if [ -w /usr/local/bin ]; then
+        sudo mv "$BINARY_NAME" /usr/local/bin/goback
+    else
+        mkdir -p ~/bin
+        mv "$BINARY_NAME" ~/bin/goback
+        echo "Installed to ~/bin. Make sure ~/bin is in your PATH."
+    fi
 else
+    # For Windows, assume Git Bash or similar, install to ~/bin
     mkdir -p ~/bin
-    mv goback ~/bin/
+    mv "$BINARY_NAME" ~/bin/goback.exe
     echo "Installed to ~/bin. Make sure ~/bin is in your PATH."
 fi
 
